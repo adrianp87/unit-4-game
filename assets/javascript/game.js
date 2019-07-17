219b233d7bc1fee368e48586fd5736da06ccc951
Adrian Perez <adrian.perez87@gmail.com>
2019-07-16 21:08:36 -0700

    added styling

---
+++ assets/javascript/game.js
@@ -0,0 +1,22 @@
+let randomNumberGenerator = Math.floor((Math.random() * 1000))
+
+
+
+// On click, generate random number
+
+
+// Adds points to player's total
+
+// Only shows amount when crystal is clicked
+// Then updates score
+
+// If total score matches random number, player wins
+
+// player loses if score goes above random number
+
+// game restarts if player loses
+
+// new values given to crystals. Must reset counter to zero
+
+
+// keep tally of games won/lost
